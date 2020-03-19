43437741f5d16efebbab0da115f951309106bdf9
LGG <lgasteiger@gmail.com>
2020-03-18 22:18:46 -0700

    First commit

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let name;
+	const myName = 'lgg';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h2>{myName.toUpperCase()} has been learning along side you too!!</h2>
 </main>
 
 <style>
