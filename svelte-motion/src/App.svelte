bbec9af51c28281a38de259dec9343e81d247023
LGG <lgasteiger@gmail.com>
2020-04-05 21:08:17 -0700

    Motion Tweened example

---
+++ svelte-motion/src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+  import TweenedAnimation from './TweenedAnimation.svelte';
+
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <table>
+	  <caption>Svelte Motion Tutorial</caption>
+		<thead>
+		  <tr>
+			  <td>Svelte Motion Concepts</td>
+				<td>Svelte Example</td>
+			</tr>
+		</thead>
+		<tbody>
+		  <tr>
+			  <td>Tweened Animation</td>
+				<td><TweenedAnimation /></td>
+			</tr>
+		</tbody>
+	</table>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	table caption {
+		padding: 0 0 0 0.5em 0;
+		font-size: 1.25em;
+	}
+
+	table thead {
+    background-color: #06c;
+		font-weight: bold;
+	}
+
+	td {
+		border: 1px solid #000;
+		padding: 0.5em 1em 0.5em 1em;
+		text-align: center;
+	}
+</style>
