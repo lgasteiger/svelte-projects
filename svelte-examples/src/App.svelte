936f211eab269a52a4c8b1c5cabe1e7e9598ddd0
LGG <lgasteiger@gmail.com>
2020-04-30 22:32:43 -0700

    Svelte example exercises

---
+++ svelte-examples/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Counter from './Counter.svelte';
+	import TemperatureConverter from './TemperatureConverter.svelte';
+	
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <h2>Svelte Examples</h2>
+
+	<h3>Counter Exercise</h3>
+	<Counter />
+
+	<h3>Temperature Converter Excercise</h3>
+	<TemperatureConverter />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-examples/src/Counter.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  function handleIncrement() {
+    count += 1;
+  } //end handleIncrement()
+</script>
+
+<!-- https://github.com/eugenkiss/7guis/wiki#counter -->
+<input
+  type=number
+  bind:value={count}
+>
+
+<button on:click="{handleIncrement}">
+  Count
+</button>
