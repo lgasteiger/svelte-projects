06debac8782f950cc51a06d8d551590e5c29a100
LGG <lgasteiger@gmail.com>
2020-05-01 22:03:25 -0700

    Svelte examples after tutorial

--- svelte-examples/src/App.svelte
+++ svelte-examples/src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Counter from './Counter.svelte';
 	import TemperatureConverter from './TemperatureConverter.svelte';
-	
+	import FlightBooker from './FlightBooker.svelte';
+	import Timer from './Timer.svelte';
+  import Crud from './Crud.svelte';		
 	export let name;
 </script>
 
@@ -9,13 +11,37 @@
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
-  <h2>Svelte Examples</h2>
-
-	<h3>Counter Exercise</h3>
-	<Counter />
-
-	<h3>Temperature Converter Excercise</h3>
-	<TemperatureConverter />
+  <table>
+	  <caption>Svelte Examples</caption>
+		<thead>
+		  <tr>
+			  <td>Svelte Concept</td>
+				<td>Svelte Project</td>
+			</tr>
+		</thead>
+		<tbody>
+		  <tr>
+			  <td>Counter Exercise</td>
+				<td><Counter /></td>
+			</tr>
+			<tr>
+			  <td>Temperature Converter Excercise</td>
+				<td><TemperatureConverter /></td>
+			</tr>
+			<tr>
+			  <td>Flight Booker Exercise</td>
+				<td><FlightBooker /></td>
+			</tr>
+			<tr>
+			  <td>Timer Exercise</td>
+				<td><Timer /></td>
+			</tr>
+			<tr>
+			  <td>CRUD Exercise</td>
+				<td><Crud /></td>
+			</tr>
+		</tbody>
+	</table>
 </main>
 
 <style>
@@ -37,5 +63,21 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	table caption {
+		padding: 0 0 0.5em 0;
+		font-size: 1.25em;
+	}
+
+	table thead {
+	  background-color: #06c;
+		font-weight: bold;	
+	}
+
+	td {
+		border: 1px solid #000;
+		padding: 0.5em 1em 0.5em 1em;
+		text-align: center;
 	}
 </style>
---
+++ svelte-examples/src/CircleDrawer.svelte
@@ -0,0 +1,11 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks#circle
+
+Click on the canvas to draw a circle. Click on a circle
+to select it. Right-click on the canvas to adjust the
+radius of the selected circle.
+-->
+
+<script>
+
+</script>
