f4ee0d97a1349a5d46b11efc05276b4791ded505
LGG <lgasteiger@gmail.com>
2020-05-19 22:05:43 -0700

    Svelte Reactivity of updating arrays

--- svelte-reactivity/src/UpdatingObjects.svelte
+++ svelte-reactivity/src/UpdatingObjects.svelte
@@ -7,9 +7,37 @@
   let numbers = [1,2,3,4];
   $: sum = numbers.reduce((t,n) => t + n, 0);
 
-  function addNumbers() {
+  function addNumber() {
     numbers[numbers.length] = numbers.length + 1;
   } //end addNumbers
+
+  function delNumber() {
+    if (numbers.length > 0) {
+      let n = numbers.pop();
+      numbers = numbers;
+      console.log(`The popped number from the array is: ${n}`);
+    } else {
+      alert('Sorry, no numbers to remove.');
+      console.log('Array is empty; no numbers to remove.');
+    } //end if
+  } //end delNumber
+
+  function delFromBeginning() {
+    if (numbers.length > 0) {
+      let n = numbers.shift();
+      numbers = numbers;
+      console.log(`The number removed is: ${n}`);
+    } else {
+      alert('Sorry, no numbers to remove.');
+      console.log('Array is empty; no numbers to remove.');
+    } //end if
+  } //end addToBeginning()
+
+  function addToBeginning() {
+    let n = numbers.length - 1;
+    numbers.unshift(n);
+    numbers = numbers;
+  } //end addToBeginning
 </script>
 
 <section class="heading">
@@ -17,11 +45,23 @@
 </section>
 <section class="content">
   <p>
+    This Webpage shall display the Reactivity ==> Updating Arrays and Objects exercise.
+  </p>
+  <p>
     {numbers.join(' + ')} = {sum}
   </p>
 
-  <button on:click={addNumbers}>
+  <button on:click={addNumber}>
     Add Number
+  </button>
+  <button on:click={delNumber}>
+    Delete Number
+  </button>
+  <button on:click={delFromBeginning}>
+    Remove From Beginning
+  </button>
+  <button on:click={addToBeginning}>
+    Add to Beginning
   </button>
 </section>
 <footer>
