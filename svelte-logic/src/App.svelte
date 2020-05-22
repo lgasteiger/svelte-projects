4b62f0b0e57fd70ac741313eda901eb22ff2b435
LGG <lgasteiger@gmail.com>
2020-05-21 18:28:07 -0700

    Svelte Logic exercises.

---
+++ svelte-logic/src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import IfBlocks from './IfBlocks.svelte';
+</script>
+
+<main>
+  <table>
+	  <caption>Svelte Tutorial Exercises</caption>
+		<thead>
+		  <tr>
+			  <td>Svelte Exercise</td>
+				<td>Svelte Project</td>
+			</tr>
+		</thead>
+		<tbody>
+		  <tr>
+			  <td class="exerciseName">Logic ==> If Blocks</td>
+        <td><IfBlocks user={{ loggedIn: false }}/></td>
+			</tr>
+		</tbody>
+	</table>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	.exerciseName {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 1em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	table caption {
+		padding: 0 0 0.5em 0;
+		font-size: 1.25em;
+	}
+
+	table thead {
+		background-color: #06c;
+		font-weight: bold;
+	}
+
+	td {
+		border: 1px solid #000;
+		padding: 0.5em 1em 0.5em 1em;
+		text-align: center;
+	}
+</style>
---
+++ svelte-logic/src/IfBlocks.svelte
@@ -0,0 +1,50 @@
+<svelte:head>
+  <link rel="stylesheet" href="./svelteIntro.css">
+</svelte:head>
+
+<script>
+  const footerImage = './images/machine-learning-playlist.png';
+
+  export let user;
+
+  function toggle() {
+    user.loggedIn = !user.loggedIn;
+  } //end toggle()
+</script>
+
+<section class="heading">
+</section>
+<section class="content">
+  <p>
+    This Webpage shall display the Svelte Logic If Blocks exercises.
+  </p>
+  {#if user.loggedIn}
+    <button on:click={toggle}>
+      Log Out
+    </button>
+  {/if}
+  {#if !user.loggedIn}
+    <button on:click={toggle}>
+      Log In
+    </button>
+  {/if}
+</section>
+<footer>
+  <figure>
+    <img
+      src={footerImage}
+      width="125"
+      height="75"
+      alt="Footer of machine learning."
+    >
+    <figcaption>
+      <a href="https://svelte.dev/">
+        Svelte JS Info
+      </a>
+    </figcaption>
+  </figure>
+  <p>
+    This Webpage is HTML5 compliant.<br />
+    Last Updated: 2020-05-11
+  </p>
+</footer>
